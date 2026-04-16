c7b89241336c62c75796144d2b115258d23dc422
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-04-16 09:05:18 +0300

    start new project

--- app/Dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const dashboard = () => {
-  return (
-    <div>dashbaord page</div>
-  )
-}
-
-export default dashboard
--- app/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const admin = () => {
-  return (
-    <div>Admin PAGE</div>
-  )
-}
-
-export default admin
